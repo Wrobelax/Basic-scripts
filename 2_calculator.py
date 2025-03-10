e303fec6b8943d7a2b77a885be560c0facea71bd
Wrobelax <awrobel.wr@gmail.com>
2025-03-10 15:59:06 +0100

    Add files via upload

---
+++ 2_calculator.py
@@ -0,0 +1,12 @@
+# This is a script for asking a user for numbers and returning simple equations
+
+a = float(input(f'Please input first number: ')) #changing to floats when input
+b = float(input(f'Please input second number: '))
+
+print('Sum of your numbers is', a + b)
+print('Deduction of your numbers is', a - b)
+print('Multiplication of your numbers is', a * b)
+print('Division of your numbers is', a / b)
+print('First number to the power of second is', a ** b)
+print('Modulo is', a % b)
+print('Floor of numbers is', a // b)
