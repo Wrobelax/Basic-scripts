3513ef69ed9ade7af380192d83f1a9d09e689f39
Wrobelax <awrobel.wr@gmail.com>
2025-03-10 15:37:42 +0100

    Update 1-name.py

--- 1-name.py
+++ 1-name.py
@@ -1,6 +1,6 @@
 # This is a script for asking a user for their name and favourite number and then printing it back to them.
 
 name = input(f'What is your name? ')
-number = input(f'What is your favourite number? ') # Left as string because an user may input non-integer number.
+number = input(f'What is your favourite number? ') # Left as string because a user may input non-integer number.
 
 print(f'Your name is {name} and your favourite number is {number}')
